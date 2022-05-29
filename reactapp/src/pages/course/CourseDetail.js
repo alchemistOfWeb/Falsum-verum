8b03d068b82a2be5b40ab59f4808b4ac07956804
alchemistOfWeb <nikitakzntcv@gmail.com>
2022-05-29 16:20:21 +0400

    implementing design of detail course page

--- reactapp/src/pages/course/CourseDetail.js
+++ reactapp/src/pages/course/CourseDetail.js
@@ -4,171 +4,37 @@ import { useParams, Link } from "react-router-dom";
 import React from 'react';
 import { useAsync } from 'react-async';
 import { Collapse, ListGroup, Nav, Container, Row, Col, Dropdown } from "react-bootstrap";
-
+import CourseSidebar from "./CourseSidebar";
 
 export default function CourseDetail() {
     return (
-        <Container fluid>
-            <Row className="flex-nowrap">
-                <div className="offcanvas-btn-sidebar">
-                    <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-dark bg-light min-vh-100">
-                        dfdf sdfsd
-                    </div>
-                </div>
-                <Col className="offcanvas-sidebar col-auto px-sm-2 px-0 bg-dark" md="3" xl="2">
-                    <div className="inner-content d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                        <Link to="/" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                            <span className="fs-5 d-none d-sm-inline">Stepus</span>
-                        </Link>
-                        <Nav variant="pills" className="flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-                            <Nav.Item>
-                                <Nav.Link href="#" className="align-middle px-0">
-                                    <i className="fs-4 bi-house"></i> <span className="ms-1 d-none d-sm-inline">Домой</span>
-                                </Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link href="#submenu1" data-bs-toggle="collapse" className="px-0 align-middle">
-                                    <i className="fs-4 bi-speedometer2"></i> 
-                                    <span className="ms-1 d-none d-sm-inline">Панель управления</span> 
-                                </Nav.Link>
-                                <Nav 
-                                    className="collapse flex-column ms-1" 
-                                    id="submenu1" 
-                                    data-bs-parent="#menu"
-                                >
-                                    <li className="w-100">
-                                        <Nav.Link href="#" className="px-0"> 
-                                            <span className="d-none d-sm-inline">Item</span> 1 
-                                        </Nav.Link>
-                                    </li>
-                                    <li>
-                                        <Nav.Link href="#" className="px-0"> 
-                                            <span className="d-none d-sm-inline">Item</span> 2 
-                                        </Nav.Link>
-                                    </li>
-                                </Nav>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link href="#" className="px-0 align-middle">
-                                    <i className="fs-4 bi-table"></i> 
-                                    <span className="ms-1 d-none d-sm-inline">Календарь</span>
-                                </Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link href="#submenu3" data-bs-toggle="collapse" className="px-0 align-middle">
-                                    {/* <i className="fs-4 bi-grid"></i>  */}
-                                    <i className="fs-4 bi-easel-fill"></i> 
-                                    <span className="ms-1 d-none d-sm-inline">Уроки</span> 
-                                </Nav.Link>
-                                <ul className="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
-                                    <li className="w-100">
-                                        <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 1</a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 2</a>
-                                    </li>
-                                </ul>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link href="/" className="px-0 align-middle">
-                                    <i className="fs-4 bi-people"></i> 
-                                    <span className="ms-1 d-none d-sm-inline">Авторы</span> 
-                                </Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link
-                                href="/" 
-                                className="px-0 align-middle"
-                                >
-                                    <i className="fs-4 bi-graph-up"></i> 
-                                    <span className="ms-1 d-none d-sm-inline">Статистика</span> 
-                                </Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link
-                                href="/" 
-                                className="px-0 align-middle"
-                                >
-                                    <i className="fs-4 bi-diagram-3-fill"></i> 
-                                    <span className="ms-1 d-none d-sm-inline">Карта курса</span>
-                                </Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link
-                                href="/courses/1" 
-                                className="px-0 align-middle active"
-                                >
-                                    <i className="fs-4 bi-info-square-fill"></i> 
-                                    <span className="ms-1 d-none d-sm-inline">Информация о курсе</span> 
-                                </Nav.Link>
-                            </Nav.Item>
-                        </Nav>
-                        <hr/>
-                        <Dropdown className="pb-4">
-                            <Dropdown.Toggle as="a" href="#" className="d-flex align-items-center text-white text-decoration-none" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                                <img 
-                                src="https://github.com/mdo.png" 
-                                alt="hugenerd" width="30" height="30" 
-                                className="rounded-circle"/>
-                                <span className="d-none d-sm-inline mx-1">User</span>
-                            </Dropdown.Toggle>
-                            <Dropdown.Menu variant="dark" className="text-small shadow" aria-labelledby="dropdownUser1">
-                                <Dropdown.Item href="#">New course</Dropdown.Item>
-                                <Dropdown.Item href="#">Settings</Dropdown.Item>
-                                <Dropdown.Item href="#">Profile</Dropdown.Item>
-                                <Dropdown.Divider/>
-                                <Dropdown.Item href="#">Sign out</Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    </div>
-                </Col>
-                <Col className="py-3">
-                    <h3>Left Sidebar with Submenus</h3>
-                    <p className="lead">
-                        An example 2-level sidebar with collasible menu items. The menu functions like an "accordion" where only a single 
-                        menu is be open at a time. While the sidebar itself is not toggle-able, it does responsively shrink in width on smaller screens.
-                    </p>
-                    <ul className="list-unstyled">
-                        <li><h5>Responsive</h5> shrinks in width, hides text labels and collapses to icons only on mobile</li>
-                    </ul>
-                    <p>
-                        An example 2-level sidebar with collasible menu items. The menu functions like an "accordion" where only a single 
-                        menu is be open at a time. While the sidebar itself is not toggle-able, it does responsively shrink in width on smaller screens.
-                    </p>
-                    <p>
-                        An example 2-level sidebar with collasible menu items. The menu functions like an "accordion" where only a single 
-                        menu is be open at a time. While the sidebar itself is not toggle-able, it does responsively shrink in width on smaller screens.
-                    </p>
-                    <p>
-                        An example 2-level sidebar with collasible menu items. The menu functions like an "accordion" where only a single 
-                        menu is be open at a time. While the sidebar itself is not toggle-able, it does responsively shrink in width on smaller screens.
-                    </p>
-                    <p>
-                        An example 2-level sidebar with collasible menu items. The menu functions like an "accordion" where only a single 
-                        menu is be open at a time. While the sidebar itself is not toggle-able, it does responsively shrink in width on smaller screens.
-                    </p>
-                    <p>
-                        An example 2-level sidebar with collasible menu items. The menu functions like an "accordion" where only a single 
-                        menu is be open at a time. While the sidebar itself is not toggle-able, it does responsively shrink in width on smaller screens.
-                    </p>
-                    <p>
-                        An example 2-level sidebar with collasible menu items. The menu functions like an "accordion" where only a single 
-                        menu is be open at a time. While the sidebar itself is not toggle-able, it does responsively shrink in width on smaller screens.
-                    </p>
-                    <p>
-                        An example 2-level sidebar with collasible menu items. The menu functions like an "accordion" where only a single 
-                        menu is be open at a time. While the sidebar itself is not toggle-able, it does responsively shrink in width on smaller screens.
-                    </p>
-                    <p>
-                        An example 2-level sidebar with collasible menu items. The menu functions like an "accordion" where only a single 
-                        menu is be open at a time. While the sidebar itself is not toggle-able, it does responsively shrink in width on smaller screens.
-                    </p>
-                    <p>
-                        An example 2-level sidebar with collasible menu items. The menu functions like an "accordion" where only a single 
-                        menu is be open at a time. While the sidebar itself is not toggle-able, it does responsively shrink in width on smaller screens.
-                    </p>
-                </Col>
-            </Row>
-        </Container>
+        <div id="course-wrapper" className="d-flex">
+            <CourseSidebar/>            
+            <div id="content" className="p-4 p-md-5 pt-3">
+                <h2 className="mb-4">Информация о курсе</h2>
+                <hr />
+                <p>
+                В этом онлайн-курсе НИУ ВШЭ мы познакомимся с базовыми понятиями статистики, научимся аккуратно собирать данные, обрабатывать их и визуализировать. Также мы поговорим про базовые теоремы, которые используются в математической статистике: ЗБЧ и ЦПТ.
+                </p>
+                <p>
+                В онлайн-курсе мы изучим основы математической статистики и аккуратную работу с данными.  
+                </p>
+                <p>
+                Мы научимся собирать и обрабатывать данные с помощью Python, поговорим про их визуализацию и предварительный анализ. 
+                </p>
+                <p>
+                Мы также познакомимся с основными распределениями и описательными статистиками, с которыми аналитики сталкиваются на повседневной основе. И обсудим теоремы, на которых базируется вся наука о данных: закон больших чисел и центральную предельную теорему.
+                </p>
+                <p>
+                Github со всеми материалами курса:  https://github.com/FUlyankin/matstat_online
+                </p>
+                <p>
+                Курс состоит из 5 недель. Каждая включает в себя несколько  коротких видеолекций (суммарная продолжительность – от 60 до 100 минут), тест на знание теоретического материала (5 – 15 вопросов), а также тест, включающий в себя выполнение заданий по программированию и решение теоретических задач. 
+                </p>
+                <p>
+                На некоторых неделях задание по программированию заменено заданием на взаимное оценивание. В конце курса предусмотрен итоговый экзамен, состоящий из тестовых вопросов.
+                </p>
+            </div>
+        </div>
     )
 }
---
+++ reactapp/src/pages/course/CourseSidebar.js
@@ -0,0 +1,134 @@
+import { useParams, Link } from "react-router-dom";
+import React from 'react';
+import { useAsync } from 'react-async';
+import { Collapse, ListGroup, Nav, Container, Row, Col, Dropdown, Offcanvas } from "react-bootstrap";
+import { useState } from "react";
+
+
+
+export default function CourseSidebar() {
+    const [show, setShow] = useState(false);
+    const toggleShow = () => setShow((s) => !s);
+
+    return (
+        <nav id="course-sidebar" className={show ? "rolled-up" : ""}>
+            <div className="custom-menu">
+                <button type="button" id="sidebarCollapse" className="btn btn-primary" onClick={toggleShow}>
+                    {/* <i className="fa fa-bars"></i> */}
+                    <i className="bi bi-list"></i>
+                    {/* <span className="sr-only">Toggle Menu</span> */}
+                </button>
+            </div>
+            <div className="p-4">
+                <Link to="/" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                    <span className="fs-5">Stepus</span>
+                </Link>
+                <Nav variant="pills" className="flex-column mb-sm-auto mb-0 align-items-start" id="menu">
+                    <Nav.Item>
+                        <Nav.Link href="#" className="align-middle px-0">
+                            <i className="fs-4 bi-house"></i> <span className="ms-1">На страницу каталога</span>
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#submenu1" data-bs-toggle="collapse" className="px-0 align-middle">
+                            <i className="fs-4 bi-speedometer2"></i> 
+                            <span className="ms-1">Панель управления</span> 
+                        </Nav.Link>
+                        <Nav 
+                            className="collapse flex-column ms-1" 
+                            id="submenu1" 
+                            data-bs-parent="#menu"
+                        >
+                            <li className="w-100">
+                                <Nav.Link href="#" className="px-0"> 
+                                    <span className="">Item</span> 1 
+                                </Nav.Link>
+                            </li>
+                            <li>
+                                <Nav.Link href="#" className="px-0"> 
+                                    <span className="">Item</span> 2 
+                                </Nav.Link>
+                            </li>
+                        </Nav>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#submenu3" data-bs-toggle="collapse" className="px-0 align-middle">
+                            {/* <i className="fs-4 bi-grid"></i>  */}
+                            <i className="fs-4 bi-easel-fill"></i> 
+                            <span className="ms-1">Уроки</span> 
+                        </Nav.Link>
+                        <ul className="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
+                            <li className="w-100">
+                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 1</a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 2</a>
+                            </li>
+                        </ul>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link
+                        href="/" 
+                        className="px-0 align-middle"
+                        >
+                            <i className="fs-4 bi-diagram-3-fill"></i> 
+                            <span className="ms-1">Карта курса</span>
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#" className="px-0 align-middle">
+                            <i className="fs-4 bi-table"></i> 
+                            <span className="ms-1">Календарь</span>
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link
+                        href="/courses/1" 
+                        className="px-0 align-middle active"
+                        >
+                            <i className="fs-4 bi-info-square-fill"></i> 
+                            <span className="ms-1">Информация о курсе</span> 
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link 
+                        href="/" 
+                        className="px-0 align-middle"
+                        >
+                            <i className="fs-4 bi-people"></i> 
+                            <span className="ms-1">Авторы</span> 
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link
+                        href="/" 
+                        className="px-0 align-middle"
+                        >
+                            <i className="fs-4 bi-graph-up"></i> 
+                            <span className="ms-1">Статистика</span> 
+                        </Nav.Link>
+                    </Nav.Item>
+                    
+                </Nav>
+
+                {/* <div className="mb-5">
+                    <h3 className="h6 mb-3">Subscribe for newsletter</h3>
+                    <form action="#" className="subscribe-form">
+                        <div className="form-group d-flex">
+                        <div className="icon">
+                            <span className="icon-paper-plane"></span>
+                        </div>
+                        <input type="text" className="form-control" placeholder="Enter Email Address"/>
+                        </div>
+                    </form>
+                </div> */}
+                {/* <div className="footer">
+                    <p>
+                    Copyright ©<script>document.write(new Date().getFullYear());</script>2022 All rights reserved | This template is made with <i className="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
+                    </p>
+                </div> */}
+            </div>
+        </nav>
+        
+    )
+}
